2dcbc59f8748276d6da2ae03d5095c6f3a801eba
evgenijgalata <dushirak@gmail.com>
2025-04-02 14:15:23 +0500

    Added method to open new page with page class methods and waiting for load page

--- base_page.py
+++ base_page.py
@@ -10,3 +10,18 @@ class BasePage:
     def goto(self, path=""):
         self.page.goto(f"{self.BASE_URL}{path}")
         return self
+
+    def page_loaded(self, method="load"):
+        self.page.wait_for_load_state(method)
+        return self
+
+    def open_page_by_click(self, selector, page_class):
+        """
+        Universal method to open new page by selector, return the new page
+        of page_class for work with its own methods
+        """
+        assert issubclass(page_class, BasePage), "Use page classes that inherit from BasePage"
+        
+        with self.page.expect_popup() as popup_info:
+            self.page.click(selector)
+        return page_class(popup_info.value).page_loaded()
